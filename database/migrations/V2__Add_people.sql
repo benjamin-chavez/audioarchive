8e394b2a0876a98f0eaeeecc6ed8eee390521f14
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-24 17:27:29 -0600

    added flyway

---
+++ database/migrations/V2__Add_people.sql
@@ -0,0 +1,7 @@
+-- database/migrations/V2__Add_people.sql
+
+
+
+insert into PERSON (ID, NAME) values (1, 'Axel');
+insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
