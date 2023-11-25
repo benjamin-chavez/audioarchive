082fa9472ac3bd875e1e553c3fb21d509a738eb5
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-24 23:56:01 -0600

    added two flyway migrations with standard naming

---
+++ apps/server/database/migrations/V1__Create_person_table.sql
@@ -0,0 +1,5 @@
+-- apps/server/database/migrations/V1__Create_person_table.sql
+CREATE TABLE PERSON(
+    ID int NOT NULL
+    , NAME varchar(100) NOT NULL
+);
---
+++ apps/server/database/migrations/V2__Add_people.sql
@@ -0,0 +1,9 @@
+-- apps/server/database/migrations/V2__Add_people.sql
+INSERT INTO PERSON(ID , NAME)
+    VALUES (1 , 'Axel');
+
+INSERT INTO PERSON(ID , NAME)
+    VALUES (2 , 'Mr. Foo');
+
+INSERT INTO PERSON(ID , NAME)
+    VALUES (3 , 'Ms. Bar');
