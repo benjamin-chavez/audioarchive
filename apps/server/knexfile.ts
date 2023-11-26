b4a1ebee7e3ee170c4a62c15fca95f89f2098f08
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-26 17:08:21 -0600

    updated to use DATABASE_HOST env variable

--- apps/server/knexfile.ts
+++ apps/server/knexfile.ts
@@ -76,12 +76,11 @@ const baseConfig = {
 const production = {
   client: 'postgresql',
   connection: {
-    // host: process.env.DATABASE_HOST,
-    host: 'audio-archive-psql-db2.cxq8xikgucfb.us-east-2.rds.amazonaws.com',
+    host: process.env.DATABASE_HOST,
+    // host: 'audio-archive-psql-db2.cxq8xikgucfb.us-east-2.rds.amazonaws.com',
     // user: process.env.DATABASE_USER,
     user: 'postgres',
     password: 'pg-secret',
-    // port: process.env.DATABASE_PORT,
     port: parseInt(process.env.DATABASE_PORT || '5432'),
     database: 'audio_archive_production',
     ssl: { rejectUnauthorized: false },
