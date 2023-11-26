d24c4053580afef4e6edcee3e2181a268fe9b662
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-26 16:40:32 -0600

    removed signed url logic from get all products

--- apps/server/src/services/product.service.ts
+++ apps/server/src/services/product.service.ts
@@ -15,10 +15,11 @@ class ProductService {
 
     const products = await ProductModel.getAllProductsWithUserDetails();
 
-    const productsWithSignedUrls =
-      await S3Service.getSignedUrlsForProducts(products);
+    // const productsWithSignedUrls =
+    //   await S3Service.getSignedUrlsForProducts(products);
 
-    return productsWithSignedUrls;
+    // return productsWithSignedUrls;
+    return products;
   }
 
   static async getAllProductsByAppUser(appUserId: number): Promise<any> {
