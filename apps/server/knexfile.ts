461d316491a333823b09f85f21b0652bac07339c
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-26 18:00:51 -0600

    loadCOnfig invocationi

--- apps/server/knexfile.ts
+++ apps/server/knexfile.ts
@@ -91,136 +91,142 @@ async function loadConfig() {
   }
 }
 
-loadConfig;
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
-console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+async function initializeKnexConfig() {
+  await loadConfig();
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
+  console.log('DATABASE_HOST: ', process.env.DATABASE_HOST);
 
-function convertCamelCaseToSnakeCase(str: string): string {
-  return str.replace(/([A-Z])/g, (match, letter) => `_${letter.toLowerCase()}`);
-}
-
-const convertSnakeCaseToCamelCase = (obj: any): any => {
-  if (typeof obj !== 'object' || obj === null) {
-    return obj;
+  function convertCamelCaseToSnakeCase(str: string): string {
+    return str.replace(
+      /([A-Z])/g,
+      (match, letter) => `_${letter.toLowerCase()}`
+    );
   }
 
-  return Object.keys(obj).reduce((accumulator, key) => {
-    const camelCaseKey = key.replace(/_([a-z])/g, (match, letter) =>
-      letter.toUpperCase()
-    );
-
-    accumulator[camelCaseKey] = convertSnakeCaseToCamelCase(obj[key]);
-
-    return accumulator;
-  }, {});
-};
-
-const baseConfig = {
-  client: 'postgresql',
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './src/database/migrations',
-    // stub: './src/database/migration.stub.js',
-    migrationSource: new CustomMigrationSource(
-      path.join(__dirname, 'migrations')
-    ),
-  },
-  seeds: {
-    directory: './src/database/seeds/development',
-    // directory: './src/database/migrations',
-  },
-  postProcessResponse: (result) => {
-    if (Array.isArray(result)) {
-      return result.map((row) => convertSnakeCaseToCamelCase(row));
-    } else {
-      return convertSnakeCaseToCamelCase(result);
+  const convertSnakeCaseToCamelCase = (obj: any): any => {
+    if (typeof obj !== 'object' || obj === null) {
+      return obj;
     }
-  },
-  wrapIdentifier: (
-    value,
-    origImpl
-    // queryContext,
-  ) => {
-    return origImpl(convertCamelCaseToSnakeCase(value));
-  },
-};
 
-// const development = {
-//   ...baseConfig,
-//   connection: {
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     port: process.env.DATABASE_PORT,
-//     database: process.env.DATABASE_NAME,
-//   },
-// };
+    return Object.keys(obj).reduce((accumulator, key) => {
+      const camelCaseKey = key.replace(/_([a-z])/g, (match, letter) =>
+        letter.toUpperCase()
+      );
 
-// host:
-//   process.env.USE_LOCAL_DB_TUNNEL === 'true'
-//     ? 'localhost'
-//     : process.env.DATABASE_HOST,
-const production = {
-  client: 'postgresql',
-  connection: {
-    // host: process.env.DATABASE_HOST,
-    host: 'audio-archive-psql-db2.cxq8xikgucfb.us-east-2.rds.amazonaws.com',
-    // user: process.env.DATABASE_USER,
-    user: 'postgres',
-    password: 'pg-secret',
-    port: parseInt(process.env.DATABASE_PORT || '5432'),
-    database: 'audio_archive_production',
-    ssl: { rejectUnauthorized: false },
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-    directory: './src/database/migrations',
-    // stub: './src/database/migration.stub.js',
-    migrationSource: new CustomMigrationSource(
-      path.join(__dirname, 'migrations')
-    ),
-  },
-  seeds: {
-    directory: './src/database/seeds/development',
-    // directory: './src/database/migrations',
-  },
-  postProcessResponse: (result) => {
-    if (Array.isArray(result)) {
-      return result.map((row) => convertSnakeCaseToCamelCase(row));
-    } else {
-      return convertSnakeCaseToCamelCase(result);
-    }
-  },
-  wrapIdentifier: (
-    value,
-    origImpl
-    // queryContext,
-  ) => {
-    return origImpl(convertCamelCaseToSnakeCase(value));
-  },
-  // ...baseConfig,
-};
+      accumulator[camelCaseKey] = convertSnakeCaseToCamelCase(obj[key]);
 
-const knexConfig = {
-  // development,
-  production,
-};
+      return accumulator;
+    }, {});
+  };
 
-export default knexConfig;
+  const baseConfig = {
+    client: 'postgresql',
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+      // stub: './src/database/migration.stub.js',
+      migrationSource: new CustomMigrationSource(
+        path.join(__dirname, 'migrations')
+      ),
+    },
+    seeds: {
+      directory: './src/database/seeds/development',
+      // directory: './src/database/migrations',
+    },
+    postProcessResponse: (result) => {
+      if (Array.isArray(result)) {
+        return result.map((row) => convertSnakeCaseToCamelCase(row));
+      } else {
+        return convertSnakeCaseToCamelCase(result);
+      }
+    },
+    wrapIdentifier: (
+      value,
+      origImpl
+      // queryContext,
+    ) => {
+      return origImpl(convertCamelCaseToSnakeCase(value));
+    },
+  };
+
+  // const development = {
+  //   ...baseConfig,
+  //   connection: {
+  //     host: process.env.DATABASE_HOST,
+  //     user: process.env.DATABASE_USER,
+  //     password: process.env.DATABASE_PASSWORD,
+  //     port: process.env.DATABASE_PORT,
+  //     database: process.env.DATABASE_NAME,
+  //   },
+  // };
+
+  // host:
+  //   process.env.USE_LOCAL_DB_TUNNEL === 'true'
+  //     ? 'localhost'
+  //     : process.env.DATABASE_HOST,
+  const production = {
+    client: 'postgresql',
+    connection: {
+      // host: process.env.DATABASE_HOST,
+      host: 'audio-archive-psql-db2.cxq8xikgucfb.us-east-2.rds.amazonaws.com',
+      // user: process.env.DATABASE_USER,
+      user: 'postgres',
+      password: 'pg-secret',
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
+      database: 'audio_archive_production',
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+      // stub: './src/database/migration.stub.js',
+      migrationSource: new CustomMigrationSource(
+        path.join(__dirname, 'migrations')
+      ),
+    },
+    seeds: {
+      directory: './src/database/seeds/development',
+      // directory: './src/database/migrations',
+    },
+    postProcessResponse: (result) => {
+      if (Array.isArray(result)) {
+        return result.map((row) => convertSnakeCaseToCamelCase(row));
+      } else {
+        return convertSnakeCaseToCamelCase(result);
+      }
+    },
+    wrapIdentifier: (
+      value,
+      origImpl
+      // queryContext,
+    ) => {
+      return origImpl(convertCamelCaseToSnakeCase(value));
+    },
+    // ...baseConfig,
+  };
+
+  const knexConfig = {
+    // development,
+    production,
+  };
+  return knexConfig;
+}
+// export default knexConfig;
+export default initializeKnexConfig();
