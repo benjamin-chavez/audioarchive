5f11edaa7835cb1288516a48099ae2d925e8faf2
Ben Chavez <ben.m.chavez@gmail.com>
2023-11-26 20:34:25 -0600

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
+// module.exports = {
+//   root: true,
+//   // This tells ESLint to load the config from the package `eslint-config-custom`
+//   extends: ['eslint-config-custom'],
+//   settings: {
+//     next: {
+//       rootDir: ['apps/*/'],
+//     },
+//   },
+// };
+
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['eslint-config-custom'],
-  settings: {
-    next: {
-      rootDir: ['apps/*/'],
-    },
-  },
+  extends: ['@repo/eslint-config/index.js'],
 };
---
+++ packages/eslint-config/server.js
@@ -0,0 +1,21 @@
+// packages/eslint-config/server.js
+
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
