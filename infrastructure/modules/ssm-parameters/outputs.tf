95805f128479d22a1c11623a25cfd6abcaf93729
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-01 13:44:55 -0600

    updated tf SSM Parameters to output parameter object

--- infrastructure/modules/ssm-parameters/outputs.tf
+++ infrastructure/modules/ssm-parameters/outputs.tf
@@ -1,133 +1,122 @@
 # infrastructure/modules/ssm-parameters/outputs.tf
-output "github_token_arn" {
-  value       = data.aws_ssm_parameter.github_token.arn
+output "github_token" {
+  value       = data.aws_ssm_parameter.github_token
   description = "The GitHub token from SSM."
   sensitive   = true
 }
 
-output "node_env_arn" {
-  value       = data.aws_ssm_parameter.node_env.arn
+output "node_env" {
+  value       = data.aws_ssm_parameter.node_env
   description = "The NODE_ENV value from SSM."
 }
 
-output "client_url_arn" {
-  value       = data.aws_ssm_parameter.client_url.arn
+output "client_url" {
+  value       = data.aws_ssm_parameter.client_url
   description = "The CLIENT_URL value from SSM."
 }
 
-output "use_local_db_tunnel_arn" {
-  value       = data.aws_ssm_parameter.use_local_db_tunnel.arn
+output "use_local_db_tunnel" {
+  value       = data.aws_ssm_parameter.use_local_db_tunnel
   description = "Flag to use local DB tunnel from SSM."
 }
 
 output "db_user" {
-  value       = data.aws_ssm_parameter.db_user.value
+  value       = data.aws_ssm_parameter.db_user
   description = "The database user from SSM."
 }
 
-output "db_user_arn" {
-  value       = data.aws_ssm_parameter.db_user.arn
-  description = "The database user ARN from SSM."
-}
-
 output "db_password" {
-  value       = data.aws_ssm_parameter.db_password.value
+  value       = data.aws_ssm_parameter.db_password
   description = "The database password from SSM."
   sensitive   = true
 }
 
-output "db_password_arn" {
-  value       = data.aws_ssm_parameter.db_password.arn
-  description = "The database password ARN from SSM."
-  sensitive   = true
-}
-
-output "db_host_arn" {
-  value       = data.aws_ssm_parameter.db_host.arn
+output "db_host" {
+  value       = data.aws_ssm_parameter.db_host
   description = "The database host from SSM."
 }
 
-output "db_port_arn" {
-  value       = data.aws_ssm_parameter.db_port.arn
+output "db_port" {
+  value       = data.aws_ssm_parameter.db_port
   description = "The database port from SSM."
 }
 
-output "db_name_arn" {
-  value       = data.aws_ssm_parameter.db_name.arn
+output "db_name" {
+  value       = data.aws_ssm_parameter.db_name
   description = "The database name from SSM."
 }
 
-output "auth0_audience_arn" {
-  value       = data.aws_ssm_parameter.auth0_audience.arn
+output "auth0_audience" {
+  value       = data.aws_ssm_parameter.auth0_audience
   description = "The Auth0 audience value from SSM."
 }
 
-output "auth0_base_url_arn" {
-  value       = data.aws_ssm_parameter.auth0_base_url.arn
+output "auth0_base_url" {
+  value       = data.aws_ssm_parameter.auth0_base_url
   description = "The Auth0 base URL from SSM."
 }
 
-output "auth0_client_id_arn" {
-  value       = data.aws_ssm_parameter.auth0_client_id.arn
+output "auth0_client_id" {
+  value       = data.aws_ssm_parameter.auth0_client_id
   description = "The Auth0 client ID from SSM."
 }
 
-output "auth0_client_secret_arn" {
-  value       = data.aws_ssm_parameter.auth0_client_secret.arn
+output "auth0_client_secret" {
+  value       = data.aws_ssm_parameter.auth0_client_secret
   description = "The Auth0 client secret from SSM."
   sensitive   = true
 }
 
-output "auth0_issuer_base_url_arn" {
-  value       = data.aws_ssm_parameter.auth0_issuer_base_url.arn
+output "auth0_issuer_base_url" {
+  value       = data.aws_ssm_parameter.auth0_issuer_base_url
   description = "The Auth0 issuer base URL from SSM."
 }
 
-output "auth0_scope_arn" {
-  value       = data.aws_ssm_parameter.auth0_scope.arn
+output "auth0_scope" {
+  value       = data.aws_ssm_parameter.auth0_scope
   description = "The Auth0 scope from SSM."
 }
 
-output "auth0_secret_arn" {
-  value       = data.aws_ssm_parameter.auth0_secret.arn
+output "auth0_secret" {
+  value       = data.aws_ssm_parameter.auth0_secret
   description = "The Auth0 secret from SSM."
   sensitive   = true
 }
 
-output "stripe_publishable_key_arn" {
-  value       = data.aws_ssm_parameter.stripe_publishable_key.arn
+output "stripe_publishable_key" {
+  value       = data.aws_ssm_parameter.stripe_publishable_key
   description = "The Stripe publishable key from SSM."
 }
 
-output "stripe_secret_key_arn" {
-  value       = data.aws_ssm_parameter.stripe_secret_key.arn
+output "stripe_secret_key" {
+  value       = data.aws_ssm_parameter.stripe_secret_key
   description = "The Stripe secret key from SSM."
   sensitive   = true
 }
 
-output "stripe_webhook_secret_arn" {
-  value       = data.aws_ssm_parameter.stripe_webhook_secret.arn
+output "stripe_webhook_secret" {
+  value       = data.aws_ssm_parameter.stripe_webhook_secret
   description = "The Stripe webhook secret from SSM."
   sensitive   = true
 }
 
-output "aws_mq_username_arn" {
-  value       = data.aws_ssm_parameter.aws_mq_username.arn
+output "aws_mq_username" {
+  value       = data.aws_ssm_parameter.aws_mq_username
   description = "The AWS MQ username from SSM."
 }
 
-output "aws_mq_password_arn" {
-  value       = data.aws_ssm_parameter.aws_mq_password.arn
+output "aws_mq_password" {
+  value       = data.aws_ssm_parameter.aws_mq_password
   description = "The AWS MQ password from SSM."
   sensitive   = true
 }
 
-output "aws_mq_broker_url_arn" {
-  value       = data.aws_ssm_parameter.aws_mq_broker_url.arn
+output "aws_mq_broker_url" {
+  value       = data.aws_ssm_parameter.aws_mq_broker_url
   description = "The AWS MQ broker URL from SSM."
 }
 
-output "aws_mq_port_arn" {
-  value       = data.aws_ssm_parameter.aws_mq_port.arn
+output "aws_mq_port" {
+  value       = data.aws_ssm_parameter.aws_mq_port
   description = "The AWS MQ port from SSM."
 }
