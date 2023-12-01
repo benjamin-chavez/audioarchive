ec3243d72a58a29b932f53f5882ad9c64beb0d3b
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-01 17:01:45 -0600

    fixed fetch cart bug in client app

--- apps/client/src/app/cart/page.tsx
+++ apps/client/src/app/cart/page.tsx
@@ -199,6 +199,13 @@ function CartItem({
 
 export default async function CartPage() {
   const res = await getMyCart();
+  if (!res.ok) {
+    return (
+      <div>
+        <p>Failed to get Cart Items</p>
+      </div>
+    );
+  }
   const { items: cartItems } = res.data;
   const subtotal = calculatePriceSubtotal(cartItems);
   const estimatedTax = calculateTaxEstimate();
