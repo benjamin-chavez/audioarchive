acbe98f5f3eb05d86ffd674e98b6b2013d5b1f62
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-04 09:57:26 -0600

    updated branch name in tf iac

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -95,7 +95,6 @@ variable "repository_branch" {
   description = "The name of branch the Github repository, which is going to trigger a new CodePipeline excecution"
   type        = string
   default     = "master"
-  # default = "clean-up-docker-files-commit-as-branch"
 }
 
 
