e73e15ecd1ef4c08309f4c5d6c5564306f9b61b1
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-04 22:36:38 -0600

    seed creation script

--- apps/server/src/database/seeds/seed-assets/app-user-avatars/test/remove-whitespace-from-filename.sh
+++ apps/server/src/database/seeds/seed-assets/app-user-avatars/test/remove-whitespace-from-filename.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
 
-for file in *; do
-  if [ -d "$file" ]; then
-    continue
-  fi
+echo "[" >seedfile.json
 
-  if [[ "$file" == *.sh ]]; then
+first=true
+
+for file in *; do
+  if [ -d "$file" ] || [ "$file" == *.sh ]; then
     continue
   fi
 
   base=${file%.*}
   extension=${file##*.}
+  # display_name="${base/-/ }"
+  display_name="$base"
+  username="${base/-/}"
+  username=$(echo "$username" | tr '[:upper:]' '[:lower:]')
 
   if [[ "$base" == *banner* ]]; then
-    nubase=$(echo "$base" | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]' | sed 's|-banner||g')-banner-seed
-    # nubase=$(echo "$base" | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]' | sed 's|banner-?|banner-seed|g')
+    avatarS3Key=$(echo "$base" | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]')-seed.${extension}
+    # nubase=$(echo "$base" | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]' | sed 's|-banner||g')-banner-seed
   else
+    avatarS3Key="${username}-avatar-seed.${extension}"
 
-    nubase=$(echo "$base" | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]')-avatar-seed
+    # avatarS3Key="${nubase}.${extension}"
+
+    # Handle comma for JSON formatting
+    if [ "$first" = true ]; then
+      first=false
+    else
+      echo "," >>seedfile.json
+    fi
+
+    # Write to seedfile.json
+    echo "  {" >>seedfile.json
+    echo "    \"auth_id\": \"auth0|$(openssl rand -hex 12)\"," >>seedfile.json
+    echo "    \"display_name\": \"$display_name\"," >>seedfile.json
+    echo "    \"first_name\": \"$(cut -d' ' -f1 <<<"$display_name")\"," >>seedfile.json
+    echo "    \"last_name\": \"$(cut -d' ' -f2- <<<"$display_name")\"," >>seedfile.json
+    echo "    \"username\": \"$username\"," >>seedfile.json
+    echo "    \"email\": \"${username}@faker-mail.dev\"," >>seedfile.json
+    echo "    \"avatarS3Key\": \"$avatarS3Key\"," >>seedfile.json
+    echo "    \"created_at\": \"currentTimestamp\"," >>seedfile.json
+    echo "    \"updated_at\": \"currentTimestamp\"" >>seedfile.json
+    echo "  }" >>seedfile.json
   fi
 
-  if [[ "$file" == *.* ]]; then
-    nuname="${nubase}.${extension}"
-  else
-    nuname="${nubase}"
-  fi
-
-  if [ "$file" != "$nuname" ]; then
-    mv "$file" "$nuname"
+  # File renaming logic
+  if [ "$file" != "$avatarS3Key" ]; then
+    mv -- "$file" "$avatarS3Key"
   fi
 done
+
+echo "]" >>seedfile.json
