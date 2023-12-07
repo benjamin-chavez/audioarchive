87ac19df3152f43e56d5060b3e1c59acb394f509
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-07 11:35:54 -0600

    messing around with spotify sdk - likely will undo these changes

--- apps/server/src/database/seeds/create-product-seeds-from-spotify-api.ts
+++ apps/server/src/database/seeds/create-product-seeds-from-spotify-api.ts
@@ -1,116 +1,148 @@
 //  apps/server/src/database/seeds/creaet-product-seeds-from-spotify-api
+import path from 'path';
+const envPath = path.resolve(__dirname, '../../../.env');
+import dotenv from 'dotenv';
+dotenv.config({ path: envPath });
 import { faker } from '@faker-js/faker';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import fs from 'fs';
+import artists from './data/app-user-seed-data-with-spotify.json';
 
-const artists = [
-  // {
-  //   auth_id: 'auth0|6519fd529745d9c63c2975ae',
-  //   display_name: 'KEEFE',
-  //   first_name: 'Evan',
-  //   last_name: 'Keefe',
-  //   username: 'keefe',
-  //   email: 'keefe.music@keefe.com',
-  //   avatarS3Key: 'keefe-avatar-seed.webp',
-  //   spotify_id: '4hXPtXUp0fPQZ2xpq3v8oU',
-  //   links: {
-  //     spotify: 'https://open.spotify.com/artist/4hXPtXUp0fPQZ2xpq3v8oU',
-  //   },
-  // },
-  // {
-  //   auth_id: 'auth0|6519fe7fe51e9baa2514460d',
-  //   display_name: 'Safety or Numbers',
-  //   first_name: 'Nate',
-  //   last_name: 'Pawelczyk',
-  //   username: 'safety-or-numbers',
-  //   email: 'router.music@router.com',
-  //   spotify_id: '1QNgopvt8ILfNVIXGU8k6g',
-  //   links: {
-  //     spotify: 'https://open.spotify.com/artist/1QNgopvt8ILfNVIXGU8k6g',
-  //   },
-  // },
-  // {
-  //   auth_id: 'auth0|6519fe7fe51e9baa2514460d',
-  //   display_name: 'Nathaniel Pavel',
-  //   first_name: 'Nathaniel',
-  //   last_name: 'Pavel',
-  //   username: 'nathaniel_pavel',
-  //   email: 'router.music@router.com',
-  //   avatarS3Key: 'nathaniel_pavel-avatar-seed.jpg',
-  //   spotify_id: null,
-  //   links: {},
-  // },
-  // {
-  //   auth_id: 'auth0|15bc5778036ff1ddd3670124',
-  //   display_name: 'AC Slater',
-  //   first_name: 'AC',
-  //   last_name: 'Slater',
-  //   username: 'ac-slater',
-  //   email: 'ac-slater@faker-mail.dev',
-  //   avatarS3Key: 'ac-slater-avatar-seed.webp',
-  //   spotify_id: '6EqFMCnVGBRNmwPlk2f3Uc',
-  //   links: {
-  //     spotify: 'https://open.spotify.com/artist/6EqFMCnVGBRNmwPlk2f3Uc',
-  //   },
-  // },
-  {
-    auth_id: 'auth0|a96bed71d217ce41b9e0f1cd',
-    display_name: 'KREAM',
-    first_name: 'KREAM',
-    last_name: '',
-    username: 'kream',
-    email: 'kream@faker-mail.dev',
-    avatarS3Key: 'kream-avatar-seed.webp',
-    spotify_id: '0DdDnziut7wOo6cAYWVZC5',
-    links: {
-      spotify: 'https://open.spotify.com/artist/0DdDnziut7wOo6cAYWVZC5',
-    },
-  },
-  // {
-  //   auth_id: 'auth0|926c18935acc534e5c0aaa22',
-  //   display_name: 'Bassjackers',
-  //   first_name: 'Bassjackers',
-  //   last_name: '',
-  //   username: 'bassjackers',
-  //   email: 'bassjackers@faker-mail.dev',
-  //   avatarS3Key: 'bassjackers-avatar-seed.webp',
-  //   spotify_id: '6xQvQwZQQuq9R3TdPNbcR8',
-  //   links: {
-  //     spotify: 'https://open.spotify.com/artist/6xQvQwZQQuq9R3TdPNbcR8',
-  //   },
-  // },
-  // {
-  //   auth_id: 'auth0|eaf3b0df387a1e36b7b407c4',
-  //   display_name: 'Ben Miller (Aus)',
-  //   first_name: 'Ben',
-  //   last_name: 'Miller (Aus)',
-  //   username: 'ben-miller-(aus)',
-  //   email: 'ben-miller-(aus)@faker-mail.dev',
-  //   avatarS3Key: 'ben-miller-(aus)-avatar-seed.webp',
-  //   spotify_id: null,
-  //   links: {},
-  // },
-  // {
-  //   auth_id: 'auth0|650caf196371a502e0233912',
-  //   display_name: 'Amin Chavez',
-  //   first_name: 'Amin',
-  //   last_name: 'Chavez',
-  //   username: 'amin-chavez',
-  //   email: 'aminchavez.music@gmail.com',
-  //   avatarS3Key: 'amin-chavez-avatar-seed.jpeg',
-  //   spotify_id: '1HBoMknv2KI9eI7tTnb6vZ',
-  //   links: {
-  //     spotify: 'https://open.spotify.com/artist/1HBoMknv2KI9eI7tTnb6vZ',
-  //   },
-  // },
-];
-// import artists from './data/app-user-seed-data-with-spotify.json';
+if (!process.env.SPOTIFY_CLIENT_ID || !process.env.SPOTIFY_CLIENT_SECRET) {
+  console.error(
+    'Missing required environment variables: SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET'
+  );
+  process.exit(1);
+}
+
+let spotifyClient;
+try {
+  spotifyClient = SpotifyApi.withClientCredentials(
+    process.env.SPOTIFY_CLIENT_ID,
+    process.env.SPOTIFY_CLIENT_SECRET
+  );
+} catch (error) {
+  console.error('Error initializing Spotify client:', error);
+  process.exit(1);
+}
+
+console.log(spotifyClient);
+// const artists = [
+//   // {
+//   //   auth_id: 'auth0|6519fd529745d9c63c2975ae',
+//   //   display_name: 'KEEFE',
+//   //   first_name: 'Evan',
+//   //   last_name: 'Keefe',
+//   //   username: 'keefe',
+//   //   email: 'keefe.music@keefe.com',
+//   //   avatarS3Key: 'keefe-avatar-seed.webp',
+//   //   spotify_id: '4hXPtXUp0fPQZ2xpq3v8oU',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/4hXPtXUp0fPQZ2xpq3v8oU',
+//   //   },
+//   // },
+//   // {
+//   //   auth_id: 'auth0|6519fe7fe51e9baa2514460d',
+//   //   display_name: 'Safety or Numbers',
+//   //   first_name: 'Nate',
+//   //   last_name: 'Pawelczyk',
+//   //   username: 'safety-or-numbers',
+//   //   email: 'router.music@router.com',
+//   //   spotify_id: '1QNgopvt8ILfNVIXGU8k6g',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/1QNgopvt8ILfNVIXGU8k6g',
+//   //   },
+//   // },
+//   // {
+//   //   auth_id: 'auth0|6519fe7fe51e9baa2514460d',
+//   //   display_name: 'Nathaniel Pavel',
+//   //   first_name: 'Nathaniel',
+//   //   last_name: 'Pavel',
+//   //   username: 'nathaniel_pavel',
+//   //   email: 'router.music@router.com',
+//   //   avatarS3Key: 'nathaniel_pavel-avatar-seed.jpg',
+//   //   spotify_id: null,
+//   //   links: {},
+//   // },
+//   // {
+//   //   auth_id: 'auth0|15bc5778036ff1ddd3670124',
+//   //   display_name: 'AC Slater',
+//   //   first_name: 'AC',
+//   //   last_name: 'Slater',
+//   //   username: 'ac-slater',
+//   //   email: 'ac-slater@faker-mail.dev',
+//   //   avatarS3Key: 'ac-slater-avatar-seed.webp',
+//   //   spotify_id: '6EqFMCnVGBRNmwPlk2f3Uc',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/6EqFMCnVGBRNmwPlk2f3Uc',
+//   //   },
+//   // },
+//   // {
+//   //   auth_id: 'auth0|a96bed71d217ce41b9e0f1cd',
+//   //   display_name: 'KREAM',
+//   //   first_name: 'KREAM',
+//   //   last_name: '',
+//   //   username: 'kream',
+//   //   email: 'kream@faker-mail.dev',
+//   //   avatarS3Key: 'kream-avatar-seed.webp',
+//   //   spotify_id: '0DdDnziut7wOo6cAYWVZC5',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/0DdDnziut7wOo6cAYWVZC5',
+//   //   },
+//   // },
+//   // {
+//   //   auth_id: 'auth0|926c18935acc534e5c0aaa22',
+//   //   display_name: 'Bassjackers',
+//   //   first_name: 'Bassjackers',
+//   //   last_name: '',
+//   //   username: 'bassjackers',
+//   //   email: 'bassjackers@faker-mail.dev',
+//   //   avatarS3Key: 'bassjackers-avatar-seed.webp',
+//   //   spotify_id: '6xQvQwZQQuq9R3TdPNbcR8',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/6xQvQwZQQuq9R3TdPNbcR8',
+//   //   },
+//   // },
+//   // {
+//   //   auth_id: 'auth0|eaf3b0df387a1e36b7b407c4',
+//   //   display_name: 'Ben Miller (Aus)',
+//   //   first_name: 'Ben',
+//   //   last_name: 'Miller (Aus)',
+//   //   username: 'ben-miller-(aus)',
+//   //   email: 'ben-miller-(aus)@faker-mail.dev',
+//   //   avatarS3Key: 'ben-miller-(aus)-avatar-seed.webp',
+//   //   spotify_id: null,
+//   //   links: {},
+//   // },
+//   // {
+//   //   auth_id: 'auth0|650caf196371a502e0233912',
+//   //   display_name: 'Amin Chavez',
+//   //   first_name: 'Amin',
+//   //   last_name: 'Chavez',
+//   //   username: 'amin-chavez',
+//   //   email: 'aminchavez.music@gmail.com',
+//   //   avatarS3Key: 'amin-chavez-avatar-seed.jpeg',
+//   //   spotify_id: '1HBoMknv2KI9eI7tTnb6vZ',
+//   //   links: {
+//   //     spotify: 'https://open.spotify.com/artist/1HBoMknv2KI9eI7tTnb6vZ',
+//   //   },
+//   // },
+// ];
+
+(async () => {
+  const items = await spotifyClient.search('The Beatles', ['artist']);
+  console.log(JSON.stringify(items, null, 2));
+  // console.table(
+  //   items.artists.items.map((item) => ({
+  //     name: item.name,
+  //     followers: item.followers.total,
+  //     popularity: item.popularity,
+  //   }))
+  // );
+})();
 
 const SPOTIFY_TOKEN =
-  'BQAIMrCK9_6dMOO2ZHUn5gj1rd0jVJHEZB101N3rKpqZ0xmXQkEcZlqcI9DKGx9rpkIdtpjQSvNIDyscY7po24KTRCK4kcQgzPRrhqvMDTbgLHggEYA';
-
-// curl --request GET \
-// --url 'https://api.spotify.com/v1/audio-features/6uoVKMfX6e1NcwZbzT584Y' \
-// --header 'Authorization: Bearer BQCm59Lg7WcL0IpnOtBjK3KmGRX73Z1OsT-2MIz3RTq6URaT3i23xqi71hGoDw1045af2YzvL_-GTa2C8PkXQcAI2eMlaL2G4EckzKrg85x7C8jc5zY'
+  'BQB9uUoXLvKIc332Zzb8W0yxaB6ZRyze_UDbV_ngrLDhcdkXqaO-hu0_oAM9PsKlrlnC4iSpST_XrwldYnarb3Jc4oZxR1DZ1Wpfq-plJJry8Q_7YS0';
 
 const musicalKeys: Record<number, string> = {
   0: 'C',
@@ -127,45 +159,68 @@ const musicalKeys: Record<number, string> = {
   11: 'B',
 };
 
+// const getTracksByArtist = async (artist) => {
+//   const spotifyArtistId = artist.spotify_id;
+
+//   const res = await fetch(
+//     `https://api.spotify.com/v1/artists/${spotifyArtistId}/top-tracks?market=US`,
+//     {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${SPOTIFY_TOKEN}`,
+//       },
+//     }
+//   );
+
+//   const { tracks }: any = await res.json();
+
+//   if (!tracks) {
+//     return;
+//   }
+
+//   const updatedTracks = await Promise.allSettled(
+//     tracks.map(async (track) => {
+//       return {
+//         name: track.name,
+//         id: track.id,
+//         imageUrl: track.album.images[0].url,
+//         artistId: spotifyArtistId,
+//       };
+//     })
+//   );
+
+//   return updatedTracks.map((res) => {
+//     // @ts-ignore
+//     return res.value;
+//   });
+// };
+
 const getTracksByArtist = async (artist) => {
   const spotifyArtistId = artist.spotify_id;
 
-  const res = await fetch(
-    `https://api.spotify.com/v1/artists/${spotifyArtistId}/top-tracks?market=US`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${SPOTIFY_TOKEN}`,
-      },
+  try {
+    // Use SDK method to get top tracks of an artist
+    const data = await spotifyClient.artists.getTopTracks(
+      spotifyArtistId,
+      'US'
+    );
+
+    // Check if tracks are available
+    if (!data.body.tracks) {
+      return;
     }
-  );
 
-  const { tracks }: any = await res.json();
-
-  if (!tracks) {
-    return;
+    // Map the tracks to your desired format
+    return data.body.tracks.map((track) => ({
+      name: track.name,
+      id: track.id,
+      imageUrl: track.album.images[0].url,
+      artistId: spotifyArtistId,
+    }));
+  } catch (error) {
+    console.error('Error fetching tracks:', error);
+    throw error; // or handle error as needed
   }
-
-  const updatedTracks = await Promise.allSettled(
-    tracks.map(async (track) => {
-      const name = track.name;
-      const id = track.id;
-      // const imageUrl = track.images ? track.album.images[0].url : '';
-      const imageUrl = track.album.images[0].url;
-
-      return {
-        name,
-        id,
-        imageUrl,
-        artistId: spotifyArtistId,
-      };
-    })
-  );
-
-  return updatedTracks.map((res) => {
-    // @ts-ignore
-    return res.value;
-  });
 };
 
 const getTrackAudioFeatures = async (tracks: Object[]) => {
@@ -309,38 +364,38 @@ const formatTrackObject = (tracks: Object[]) => {
   return updatedTracks;
 };
 
-(async () => {
-  const output = [];
+// (async () => {
+//   const output = [];
 
-  for (const artist of artists) {
-    console.log('output', output.length);
-    let tracks: Object[];
-    tracks = await getTracksByArtist(artist);
+//   for (const artist of artists) {
+//     console.log('output', output.length);
+//     let tracks: Object[];
+//     tracks = await getTracksByArtist(artist);
 
-    if (!tracks) {
-      continue;
-    }
-    console.log('after getTracksByArtist', tracks.length);
-    tracks = await getTrackAudioFeatures(tracks);
-    console.log('after getTrackAudioFeatures(tracks)', tracks.length);
-    tracks = await assignGenresToTracks(tracks, artist);
-    console.log('after assignGenresToTracks(tracks, artist)', tracks.length);
-    tracks = assignSoftwareToTracks(tracks);
-    console.log('after assignSoftwareToTracks(tracks)', tracks.length);
-    tracks = assignRandomPrice(tracks);
-    console.log('after assignRandomPrice(tracks)', tracks.length);
-    tracks = formatTrackObject(tracks);
-    console.log('after formatTrackObject(tracks)', tracks.length);
-    // console.log(tracks);
+//     if (!tracks) {
+//       continue;
+//     }
+//     // console.log('after getTracksByArtist', tracks.length);
+//     // tracks = await getTrackAudioFeatures(tracks);
+//     // console.log('after getTrackAudioFeatures(tracks)', tracks.length);
+//     // tracks = await assignGenresToTracks(tracks, artist);
+//     // console.log('after assignGenresToTracks(tracks, artist)', tracks.length);
+//     // tracks = assignSoftwareToTracks(tracks);
+//     // console.log('after assignSoftwareToTracks(tracks)', tracks.length);
+//     // tracks = assignRandomPrice(tracks);
+//     // console.log('after assignRandomPrice(tracks)', tracks.length);
+//     // tracks = formatTrackObject(tracks);
+//     // console.log('after formatTrackObject(tracks)', tracks.length);
+//     console.log(tracks);
 
-    output.push(tracks);
-  }
+//     output.push(tracks);
+//   }
 
-  const jsonData = JSON.stringify(output, null, 2);
-  // console.log(output);
+//   const jsonData = JSON.stringify(output, null, 2);
+//   // console.log(output);
 
-  const filename =
-    './apps/server/src/database/seeds/data/product-seed-data-with-spotify-data.json';
-  fs.writeFileSync(filename, jsonData, 'utf8');
-  console.log('data printed to: ', filename);
-})();
+//   const filename =
+//     './apps/server/src/database/seeds/data/product-seed-data-with-spotify-data.json';
+//   fs.writeFileSync(filename, jsonData, 'utf8');
+//   console.log('data printed to: ', filename);
+// })();
