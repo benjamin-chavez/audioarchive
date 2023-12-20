0776dcca28b5559e2d61645825cb799c8ae93b08
Ben Chavez <ben.m.chavez@gmail.com>
2023-12-20 01:49:24 -0600

    updated user profile page

--- apps/client/src/app/(app-users)/[username]/page.tsx
+++ apps/client/src/app/(app-users)/[username]/page.tsx
@@ -28,45 +28,50 @@ export default async function UserDetail({ params }: AppUserProps) {
         />
       </div>
       <Container>
-        <div>
-          <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-            <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-              <div className="flex">
-                <Image
-                  className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56 object-cover ring-4 ring-white"
-                  width={500}
-                  height={500}
-                  quality={100}
-                  src={appUser.avatarS3Url}
-                  alt=""
-                />
-              </div>
-              <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-                <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
-                  <h1 className="truncate text-2xl font-bold text-gray-200">
-                    {appUser.username}
-                  </h1>
-                </div>
-                <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                  >
-                    <span>Follow</span>
-                  </button>
-                </div>
-              </div>
+        {/* <div> */}
+        <div
+          // max-w-7xl
+          className="mx-auto px-4  sm:px-6 lg:px-8"
+        >
+          <div className="-mt-12 sm:-mt-24 sm:flex sm:items-end sm:space-x-5">
+            <div className="flex">
+              <Image
+                className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56 object-cover ring-4 ring-white"
+                width={500}
+                height={500}
+                quality={100}
+                src={appUser.avatarS3Url}
+                alt=""
+              />
             </div>
-            <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
-              <h1 className="truncate text-2xl font-bold text-gray-900">
-                {appUser.username}
-              </h1>
+            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+              <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+                <h1 className="truncate text-2xl font-bold text-gray-200">
+                  {appUser.username}
+                </h1>
+              </div>
+              <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <button
+                  type="button"
+                  className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  <span>Follow</span>
+                </button>
+              </div>
             </div>
           </div>
+          <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+            <h1 className="truncate text-2xl font-bold text-gray-900">
+              {appUser.username}
+            </h1>
+          </div>
         </div>
+        {/* </div> */}
 
         {products.length ? (
-          <ProductsGrid products={products} appUser={appUser} />
+          <Container>
+            <ProductsGrid products={products} appUser={appUser} />
+          </Container>
         ) : (
           <div className="pt-20">
             <h3>No Products</h3>
